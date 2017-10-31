1342284d85de8922a5045b3b60576d14cecc7e32
mohammaduzair9 <mohduzair9@gmail.com>
2017-10-31 23:42:54 +0500

    Added find_face.py file for recognizing face stored by calculating euclidean distance

--- src/web_facenet.py
+++ src/web_facenet.py
@@ -6,6 +6,7 @@ import web
 import os
 import time
 import add_face
+import find_face
 import facenet
 
 #declare variables
@@ -14,11 +15,15 @@ class_names=np.load('classnames.npy')
 embeds=np.load('embed.npy')
 labels=np.load('labels.npy')
 image_dir='static/images/'
+find_dir='static/find_face/'
 
 render = web.template.render('./templates/')
 urls = ('/', 'index')
 	
+
+print("again")
 app = web.application(urls, globals())
+
 
 #initialize tensorflow grpah
 graph = tf.Graph()   
@@ -28,6 +33,9 @@ with graph.as_default():
 	sess_config = tf.ConfigProto()
         sess_config.gpu_options.per_process_gpu_memory_fraction = 0.90
         with tf.Session(config=sess_config) as sess:
+		
+		
+		
             	
 		# Load the model
                 print('Loading feature extraction model')
@@ -60,25 +68,35 @@ with graph.as_default():
         			        		f.write(content)
         	    	
     					#add face to database/numpy
-					global class_names 
-					global labels
-					global embeds
 					st = time.time()					
-					updates=add_face.main(sess, graph, image_dir, class_names, labels, embeds)
- 					class_names=updates[0]
-					labels=updates[1]
-					embeds=updates[2]
-					
+					add_face.main(sess, graph, image_dir, class_names, labels, embeds)
+ 					
 					faces=None
 					find_image=None
 					add_image=None
 					
 				
+				# if recognize face form submit
+				else :
+					#save image to find_face directory
+					files = web.webapi.rawinput().get('file2')
+					content = files.file.read()
+					target = find_dir + files.filename					
+					with open(target, 'w') as f:
+        			        	f.write(content)
+        				
+					# find face name from database
+					st = time.time()
+					faces=find_face.main(sess, graph, target, class_names, labels, embeds)
+					print('Total time = {}'.format(time.time() - st))					
+					add_image=None
+					find_image=target
+					
+					 
 				return render.index(faces, add_image, find_image)
 
 if __name__=='__main__':
-
-	print('name again')
 	app.run()
 	
+ 		
  		
